40536e8ffce0947402db3a800fb48542656ea939
Drew Peterson <drew.a.peterson@hotmail.com>
2021-01-28 00:26:52 -0800

    Added more tests for music-artist.  The constructors all tested

--- __tests__/music-artist.test.js
+++ __tests__/music-artist.test.js
@@ -1,7 +1,6 @@
 import { MusicDB, Artist, Album } from '../src/music-artist.js';
 
 describe(MusicDB, () => {
-
   test('should create a new MusicDB object that is not null', () => {
     const testDb = new MusicDB();
     expect(testDb).not.toBeNull();
@@ -23,7 +22,43 @@ describe(MusicDB, () => {
     expect(testDb.artistId).toEqual(0);
   });
   test('should create a new MusicDB object with an initial albumId of 0', () => {
-    const testDb = new MusicDB();
-    expect(testDb.albumId).toEqual(0);
+    const testArtist = new MusicDB();
+    expect(testArtist.albumId).toEqual(0);
+  });
+});
+
+describe(Artist, () => {
+  test('should create a new Artist object that is not null', () => {
+    const testArtist = new Artist("Yes", "Classic Rock");
+    expect(testArtist).not.toBeNull();
+  });
+  test('should create a new Artist object that is not undefined', () => {
+    const testArtist = new Artist("Yes", "Classic Rock");
+    expect(testArtist).not.toBeUndefined();
+  });
+  test('should create a new Artist object with 2 arguments passed in', () => {
+    const testArtist = new Artist("Yes", "Classic Rock");
+    expect(testArtist.artist).toEqual("Yes");
+    expect(testArtist.artistGenre).toEqual("Classic Rock");
+  });    
+});
+
+describe(Album, () => {
+  test('should create a new Album object that is not null', () => {
+    const testAlbum = new Album("Yes", "90125", "1983", "Classic Rock", "Studio");
+    expect(testAlbum).not.toBeNull();
+  });
+  test('should create a new Album object that is not undefined', () => {
+    const testAlbum = new Album("Yes", "90125", "1983", "Classic Rock", "Studio");
+    expect(testAlbum).not.toBeUndefined();
+  });
+  test('should create a new Album object with 5 arguments passed in', () => {
+    const testAlbum = new Album("Yes", "90125", "1983", "Classic Rock", "Studio");
+    expect(testAlbum.albumArtist).toEqual("Yes");
+    expect(testAlbum.albumName).toEqual("90125");
+    expect(testAlbum.albumYear).toEqual("1983");
+    expect(testAlbum.albumGenre).toEqual("Classic Rock");
+    expect(testAlbum.albumType).toEqual("Studio");
+    expect(testAlbum.albumRating).toEqual("");
   });
 });
